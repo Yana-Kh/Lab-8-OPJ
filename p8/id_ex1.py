cec0a65f2338b4f5e1220cacb61f774dd7ffa82a
Yana-Kh <ynakh2017@mail.ru>
2022-11-19 20:10:27 +0300

    fix

--- p8/id_ex1.py
+++ p8/id_ex1.py
@@ -5,11 +5,12 @@ if __name__ == '__main__':
     # Ввести кортеж одной строкой.
     A = tuple(map(int, input().split()))
 
-    # Найти искомую сумму.
-    ind = 0
+    # Найти искомые числа
+    f = 0
     for i, value in enumerate(A):
-        if value % 2 == 0:
-            ind = i
-            if A[i+1] % 2 == 0:
-                print(f"Найдена пара, начиная с индекса: {ind}")
-                exit(0)
+        if value % 2 == 1 and A[i+1] % 2 == 1:
+            print(f"Найдена пара, начиная с индекса: {i}")
+            f += 1
+            break
+    if f == 0:
+        print("Пары не найдены(")
